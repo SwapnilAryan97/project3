d3e8495a27367b99b0a26cbc1a6e53a72d6c731f
swapnilaryan97 <saiamthere@gmail.com>
2022-04-29 19:28:22 -0400

    update songs test

--- tests/song_test.py
+++ tests/song_test.py
@@ -4,13 +4,13 @@
 def song_page_content(client):
     response= client.get("/songs")
     assert "Upload Songs" in response.data
-
-def test_request_main_menu_links(client):
-    """This makes the index page"""
-    response = client.get("/")
     assert response.status_code == 200
     assert b'href="/about"' in response.data
     assert b'href="/welcome"' in response.data
     assert b'href="/login"' in response.data
     assert b'href="/register"' in response.data
+
+
+# def test_request_main_menu_links(client):
+#     """This makes the index page"""
 

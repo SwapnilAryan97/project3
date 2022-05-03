36631b4e0b64ca7a693407d92903ad3df006e723
swapnilaryan97 <saiamthere@gmail.com>
2022-05-03 12:26:03 -0400

    bux fix

--- tests/csv_test.py
+++ tests/csv_test.py
@@ -2,7 +2,6 @@
 
 
 def test_csv_upload(client):
-    "Tes for CSV upload after login"
+    "This tests for CSV upload after login"
     response = client.get("/songs/upload")
     assert response.status_code == 302
-
